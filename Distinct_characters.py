0eb12800e83eafb994bf96908a82d24281ff8ecd
Deepika Kakarla <96735924+DEEPIKAKAKARLA@users.noreply.github.com>
2022-07-03 02:18:06 +0530

    a=input().replace(' ','') a=a.lower() s=[] for i in a:     if i not in s:         s.append(i) print(''.join(sorted(s)))

--- Distinct_characters.py
+++ Distinct_characters.py
@@ -1,5 +1,7 @@
-x=input()
-for i in sorted(set(x)):
-    if i>='a' and i<='z':
-        if x.count(i)==1:
-            print(i,end="")
+a=input().replace(' ','')
+a=a.lower()
+s=[]
+for i in a:
+    if i not in s:
+        s.append(i)
+print(''.join(sorted(s)))
