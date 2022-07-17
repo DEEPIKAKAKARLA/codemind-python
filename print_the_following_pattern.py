072ba10421cf7363f3e00e2015ee45bd62607a64
Deepika Kakarla <96735924+DEEPIKAKAKARLA@users.noreply.github.com>
2022-07-17 18:09:44 +0530

    n=int(input()) for i in range(n):     for j in range(1,n-1):         print(j,end='')     for j in range(1,n-2):         print(j,end='')     print()

--- print_the_following_pattern.py
+++ print_the_following_pattern.py
@@ -1,5 +1,7 @@
 n=int(input())
-for i in range(1,n+1):
-    for j in range(i,n+1):
-        print(chr(65+n-i),end=' ')
+for i in range(n):
+    for j in range(1,n-1):
+        print(j,end='')
+    for j in range(1,n-2):
+        print(j,end='')
     print()
